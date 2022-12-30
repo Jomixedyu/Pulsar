5981c0cc567a7255685c7119a0fd6c21aa0b8f72
JomiXedYu <JomiXedYu@outlook.com>
2022-12-30 21:12:58 +0800

    Update Delegate.h

--- CoreLib/Delegate.h
+++ CoreLib/Delegate.h
@@ -26,6 +26,14 @@ namespace jxcorlib
 #define _CORELIB_DELEGATE_INVOKE6 (inst->*func)(_CORELIB_DELEGATE_GETVALUE(T1, 0), _CORELIB_DELEGATE_GETVALUE(T2, 1), _CORELIB_DELEGATE_GETVALUE(T3, 2), _CORELIB_DELEGATE_GETVALUE(T4, 3), _CORELIB_DELEGATE_GETVALUE(T5, 4), _CORELIB_DELEGATE_GETVALUE(T6, 5))
 #define _CORELIB_DELEGATE_INVOKE7 (inst->*func)(_CORELIB_DELEGATE_GETVALUE(T1, 0), _CORELIB_DELEGATE_GETVALUE(T2, 1), _CORELIB_DELEGATE_GETVALUE(T3, 2), _CORELIB_DELEGATE_GETVALUE(T4, 3), _CORELIB_DELEGATE_GETVALUE(T5, 4), _CORELIB_DELEGATE_GETVALUE(T6, 5), _CORELIB_DELEGATE_GETVALUE(T7, 6))
 #define _CORELIB_DELEGATE_INVOKE8 (inst->*func)(_CORELIB_DELEGATE_GETVALUE(T1, 0), _CORELIB_DELEGATE_GETVALUE(T2, 1), _CORELIB_DELEGATE_GETVALUE(T3, 2), _CORELIB_DELEGATE_GETVALUE(T4, 3), _CORELIB_DELEGATE_GETVALUE(T5, 4), _CORELIB_DELEGATE_GETVALUE(T6, 5), _CORELIB_DELEGATE_GETVALUE(T7, 6), _CORELIB_DELEGATE_GETVALUE(T8,7))
+#define _CORELIB_DELEGATE_INVOKE9 (inst->*func)(_CORELIB_DELEGATE_GETVALUE(T1, 0), _CORELIB_DELEGATE_GETVALUE(T2, 1), _CORELIB_DELEGATE_GETVALUE(T3, 2), _CORELIB_DELEGATE_GETVALUE(T4, 3), _CORELIB_DELEGATE_GETVALUE(T5, 4), _CORELIB_DELEGATE_GETVALUE(T6, 5), _CORELIB_DELEGATE_GETVALUE(T7, 6), _CORELIB_DELEGATE_GETVALUE(T8,7), _CORELIB_DELEGATE_GETVALUE(T9,8))
+#define _CORELIB_DELEGATE_INVOKE10 (inst->*func)(_CORELIB_DELEGATE_GETVALUE(T1, 0), _CORELIB_DELEGATE_GETVALUE(T2, 1), _CORELIB_DELEGATE_GETVALUE(T3, 2), _CORELIB_DELEGATE_GETVALUE(T4, 3), _CORELIB_DELEGATE_GETVALUE(T5, 4), _CORELIB_DELEGATE_GETVALUE(T6, 5), _CORELIB_DELEGATE_GETVALUE(T7, 6), _CORELIB_DELEGATE_GETVALUE(T8,7), _CORELIB_DELEGATE_GETVALUE(T9,8), _CORELIB_DELEGATE_GETVALUE(T10,9))
+#define _CORELIB_DELEGATE_INVOKE11 (inst->*func)(_CORELIB_DELEGATE_GETVALUE(T1, 0), _CORELIB_DELEGATE_GETVALUE(T2, 1), _CORELIB_DELEGATE_GETVALUE(T3, 2), _CORELIB_DELEGATE_GETVALUE(T4, 3), _CORELIB_DELEGATE_GETVALUE(T5, 4), _CORELIB_DELEGATE_GETVALUE(T6, 5), _CORELIB_DELEGATE_GETVALUE(T7, 6), _CORELIB_DELEGATE_GETVALUE(T8,7), _CORELIB_DELEGATE_GETVALUE(T9,8), _CORELIB_DELEGATE_GETVALUE(T10,9), _CORELIB_DELEGATE_GETVALUE(T11,10))
+#define _CORELIB_DELEGATE_INVOKE12 (inst->*func)(_CORELIB_DELEGATE_GETVALUE(T1, 0), _CORELIB_DELEGATE_GETVALUE(T2, 1), _CORELIB_DELEGATE_GETVALUE(T3, 2), _CORELIB_DELEGATE_GETVALUE(T4, 3), _CORELIB_DELEGATE_GETVALUE(T5, 4), _CORELIB_DELEGATE_GETVALUE(T6, 5), _CORELIB_DELEGATE_GETVALUE(T7, 6), _CORELIB_DELEGATE_GETVALUE(T8,7), _CORELIB_DELEGATE_GETVALUE(T9,8), _CORELIB_DELEGATE_GETVALUE(T10,9), _CORELIB_DELEGATE_GETVALUE(T11,10), _CORELIB_DELEGATE_GETVALUE(T12,11))
+#define _CORELIB_DELEGATE_INVOKE13 (inst->*func)(_CORELIB_DELEGATE_GETVALUE(T1, 0), _CORELIB_DELEGATE_GETVALUE(T2, 1), _CORELIB_DELEGATE_GETVALUE(T3, 2), _CORELIB_DELEGATE_GETVALUE(T4, 3), _CORELIB_DELEGATE_GETVALUE(T5, 4), _CORELIB_DELEGATE_GETVALUE(T6, 5), _CORELIB_DELEGATE_GETVALUE(T7, 6), _CORELIB_DELEGATE_GETVALUE(T8,7), _CORELIB_DELEGATE_GETVALUE(T9,8), _CORELIB_DELEGATE_GETVALUE(T10,9), _CORELIB_DELEGATE_GETVALUE(T11,10), _CORELIB_DELEGATE_GETVALUE(T12,11), _CORELIB_DELEGATE_GETVALUE(T13,12))
+#define _CORELIB_DELEGATE_INVOKE14 (inst->*func)(_CORELIB_DELEGATE_GETVALUE(T1, 0), _CORELIB_DELEGATE_GETVALUE(T2, 1), _CORELIB_DELEGATE_GETVALUE(T3, 2), _CORELIB_DELEGATE_GETVALUE(T4, 3), _CORELIB_DELEGATE_GETVALUE(T5, 4), _CORELIB_DELEGATE_GETVALUE(T6, 5), _CORELIB_DELEGATE_GETVALUE(T7, 6), _CORELIB_DELEGATE_GETVALUE(T8,7), _CORELIB_DELEGATE_GETVALUE(T9,8), _CORELIB_DELEGATE_GETVALUE(T10,9), _CORELIB_DELEGATE_GETVALUE(T11,10), _CORELIB_DELEGATE_GETVALUE(T12,11), _CORELIB_DELEGATE_GETVALUE(T13,12), _CORELIB_DELEGATE_GETVALUE(T14,13))
+#define _CORELIB_DELEGATE_INVOKE15 (inst->*func)(_CORELIB_DELEGATE_GETVALUE(T1, 0), _CORELIB_DELEGATE_GETVALUE(T2, 1), _CORELIB_DELEGATE_GETVALUE(T3, 2), _CORELIB_DELEGATE_GETVALUE(T4, 3), _CORELIB_DELEGATE_GETVALUE(T5, 4), _CORELIB_DELEGATE_GETVALUE(T6, 5), _CORELIB_DELEGATE_GETVALUE(T7, 6), _CORELIB_DELEGATE_GETVALUE(T8,7), _CORELIB_DELEGATE_GETVALUE(T9,8), _CORELIB_DELEGATE_GETVALUE(T10,9), _CORELIB_DELEGATE_GETVALUE(T11,10), _CORELIB_DELEGATE_GETVALUE(T12,11), _CORELIB_DELEGATE_GETVALUE(T13,12), _CORELIB_DELEGATE_GETVALUE(T14,13), _CORELIB_DELEGATE_GETVALUE(T15,14))
+#define _CORELIB_DELEGATE_INVOKE16 (inst->*func)(_CORELIB_DELEGATE_GETVALUE(T1, 0), _CORELIB_DELEGATE_GETVALUE(T2, 1), _CORELIB_DELEGATE_GETVALUE(T3, 2), _CORELIB_DELEGATE_GETVALUE(T4, 3), _CORELIB_DELEGATE_GETVALUE(T5, 4), _CORELIB_DELEGATE_GETVALUE(T6, 5), _CORELIB_DELEGATE_GETVALUE(T7, 6), _CORELIB_DELEGATE_GETVALUE(T8,7), _CORELIB_DELEGATE_GETVALUE(T9,8), _CORELIB_DELEGATE_GETVALUE(T10,9), _CORELIB_DELEGATE_GETVALUE(T11,10), _CORELIB_DELEGATE_GETVALUE(T12,11), _CORELIB_DELEGATE_GETVALUE(T13,12), _CORELIB_DELEGATE_GETVALUE(T14,13), _CORELIB_DELEGATE_GETVALUE(T15,14), _CORELIB_DELEGATE_GETVALUE(T16,15))
 
     template<typename T, typename TReturn>
     Object_sp InvokeInstanceFunction(T* inst, TReturn(T::* func)(), const array_list<Object_sp>& params)
@@ -91,6 +99,62 @@ namespace jxcorlib
         if constexpr (std::is_same_v<TReturn, void>) { _CORELIB_DELEGATE_INVOKE8; return nullptr; }
         else return BoxUtil::Box<TReturn>(_CORELIB_DELEGATE_INVOKE8);
     }
+    template<typename T, typename TReturn, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9>
+    Object_sp InvokeInstanceFunction(T* inst, TReturn(T::* func)(T1, T2, T3, T4, T5, T6, T7, T8, T9), const array_list<Object_sp>& params)
+    {
+        assert(params.size() == 9);
+        if constexpr (std::is_same_v<TReturn, void>) { _CORELIB_DELEGATE_INVOKE9; return nullptr; }
+        else return BoxUtil::Box<TReturn>(_CORELIB_DELEGATE_INVOKE9);
+    }
+    template<typename T, typename TReturn, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10>
+    Object_sp InvokeInstanceFunction(T* inst, TReturn(T::* func)(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10), const array_list<Object_sp>& params)
+    {
+        assert(params.size() == 10);
+        if constexpr (std::is_same_v<TReturn, void>) { _CORELIB_DELEGATE_INVOKE10; return nullptr; }
+        else return BoxUtil::Box<TReturn>(_CORELIB_DELEGATE_INVOKE10);
+    }
+    template<typename T, typename TReturn, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11>
+    Object_sp InvokeInstanceFunction(T* inst, TReturn(T::* func)(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11), const array_list<Object_sp>& params)
+    {
+        assert(params.size() == 11);
+        if constexpr (std::is_same_v<TReturn, void>) { _CORELIB_DELEGATE_INVOKE11; return nullptr; }
+        else return BoxUtil::Box<TReturn>(_CORELIB_DELEGATE_INVOKE11);
+    }
+    template<typename T, typename TReturn, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12>
+    Object_sp InvokeInstanceFunction(T* inst, TReturn(T::* func)(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12), const array_list<Object_sp>& params)
+    {
+        assert(params.size() == 12);
+        if constexpr (std::is_same_v<TReturn, void>) { _CORELIB_DELEGATE_INVOKE12; return nullptr; }
+        else return BoxUtil::Box<TReturn>(_CORELIB_DELEGATE_INVOKE12);
+    }
+    template<typename T, typename TReturn, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13>
+    Object_sp InvokeInstanceFunction(T* inst, TReturn(T::* func)(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13), const array_list<Object_sp>& params)
+    {
+        assert(params.size() == 13);
+        if constexpr (std::is_same_v<TReturn, void>) { _CORELIB_DELEGATE_INVOKE13; return nullptr; }
+        else return BoxUtil::Box<TReturn>(_CORELIB_DELEGATE_INVOKE13);
+    }
+    template<typename T, typename TReturn, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14>
+    Object_sp InvokeInstanceFunction(T* inst, TReturn(T::* func)(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14), const array_list<Object_sp>& params)
+    {
+        assert(params.size() == 14);
+        if constexpr (std::is_same_v<TReturn, void>) { _CORELIB_DELEGATE_INVOKE14; return nullptr; }
+        else return BoxUtil::Box<TReturn>(_CORELIB_DELEGATE_INVOKE14);
+    }
+    template<typename T, typename TReturn, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14, typename T15>
+    Object_sp InvokeInstanceFunction(T* inst, TReturn(T::* func)(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15), const array_list<Object_sp>& params)
+    {
+        assert(params.size() == 15);
+        if constexpr (std::is_same_v<TReturn, void>) { _CORELIB_DELEGATE_INVOKE15; return nullptr; }
+        else return BoxUtil::Box<TReturn>(_CORELIB_DELEGATE_INVOKE15);
+    }
+    template<typename T, typename TReturn, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14, typename T15, typename T16>
+    Object_sp InvokeInstanceFunction(T* inst, TReturn(T::* func)(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16), const array_list<Object_sp>& params)
+    {
+        assert(params.size() == 16);
+        if constexpr (std::is_same_v<TReturn, void>) { _CORELIB_DELEGATE_INVOKE16; return nullptr; }
+        else return BoxUtil::Box<TReturn>(_CORELIB_DELEGATE_INVOKE16);
+    }
 #undef _CORELIB_DELEGATE_GETVALUE
 #undef _CORELIB_DELEGATE_INVOKE1
 #undef _CORELIB_DELEGATE_INVOKE2
@@ -100,13 +164,21 @@ namespace jxcorlib
 #undef _CORELIB_DELEGATE_INVOKE6
 #undef _CORELIB_DELEGATE_INVOKE7
 #undef _CORELIB_DELEGATE_INVOKE8
+#undef _CORELIB_DELEGATE_INVOKE9
+#undef _CORELIB_DELEGATE_INVOKE10
+#undef _CORELIB_DELEGATE_INVOKE11
+#undef _CORELIB_DELEGATE_INVOKE12
+#undef _CORELIB_DELEGATE_INVOKE13
+#undef _CORELIB_DELEGATE_INVOKE14
+#undef _CORELIB_DELEGATE_INVOKE15
+#undef _CORELIB_DELEGATE_INVOKE16
 
     template<typename TReturn, typename... TArgs>
     class FunctionDelegate : public Delegate
     {
         CORELIB_DEF_TEMPLATE_TYPE(AssemblyObject_jxcorlib, jxcorlib::FunctionDelegate, Delegate, TReturn, TArgs...);
 
-        static_assert(sizeof...(TArgs) <= 8);
+        static_assert(sizeof...(TArgs) <= 16);
 
         using This = FunctionDelegate<TReturn, TArgs...>;
         using FunctionType = std::function<TReturn(TArgs...)>;
