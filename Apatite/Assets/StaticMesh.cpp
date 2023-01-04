6c8b3e8a2747e6f6a2e484fc6cdd3e3cfc40bd3d
JomiXedYu <JomiXedYu@outlook.com>
2023-01-05 01:01:20 +0800

    staticmesh

---
+++ Apatite/Assets/StaticMesh.cpp
@@ -0,0 +1,137 @@
+﻿#include "StaticMesh.h"
+#include <ThirdParty/glad/glad.h>
+#include <Apatite/Assets/Shader.h>
+#include <Apatite/Assets/Texture2D.h>
+
+namespace apatite
+{
+    ser::Stream& ReadWriteStream(ser::Stream& stream, bool is_write, StaticMeshVertexData& vert)
+    {
+        math::ReadWriteStream(stream, is_write, vert.Position);
+        math::ReadWriteStream(stream, is_write, vert.Normal);
+        math::ReadWriteStream(stream, is_write, vert.Tangent);
+        math::ReadWriteStream(stream, is_write, vert.BitTangent);
+        for (size_t i = 0; i < APATITE_STATICMESH_MAX_TEXTURE_COORDS; i++)
+        {
+            math::ReadWriteStream(stream, is_write, vert.Coords[i]);
+        }
+        math::ReadWriteStream(stream, is_write, vert.VertColor);
+
+        return stream;
+    }
+
+
+    void StaticMesh::Serialize(ser::Stream& stream, bool is_ser)
+    {
+        int32_t ser_ver;
+        if (is_ser) ser_ver = SerializeVersion;
+
+        ReadWriteStream(stream, is_ser, ser_ver);
+        assert(!is_ser && ser_ver == SerializeVersion);
+
+        if (!is_ser)
+        {
+            this->raw_data_ = mksptr(new StaticMeshVertexDataArray);
+        }
+        ReadWriteStream(stream, is_ser, *this->raw_data_);
+
+
+        if (!is_ser)
+        {
+            this->BindGPU();
+        }
+    }
+
+    void StaticMesh::OnInstantiateAsset(sptr<AssetObject>& obj)
+    {
+        auto mesh = sptr_cast<StaticMesh>(obj);
+        assert(mesh);
+        mesh->raw_data_ = this->raw_data_;
+    }
+
+    void StaticMesh::Render()
+    {
+        assert(this->GetIsBindGPU());
+
+    }
+
+    StaticMesh::~StaticMesh()
+    {
+        this->UnBindGPU();
+    }
+
+    StaticMesh_sp StaticMesh::StaticCreate(sptr<StaticMeshVertexDataArray>&& managed_data)
+    {
+        auto mesh = new StaticMesh;
+
+        mesh->raw_data_ = managed_data;
+        mesh->BindGPU();
+
+        return mksptr(mesh);
+    }
+
+
+    void StaticMesh::BindGPU()
+    {
+        assert(this->GetIsBindGPU());
+
+        uint32_t& vao = this->render_handle_;
+        glGenVertexArrays(1, &vao);
+
+        uint32_t& vbo = this->render_buffer_;
+        glGenBuffers(1, &vbo);
+
+        glBindVertexArray(vao);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+        glBufferData(GL_ARRAY_BUFFER, this->raw_data_->size() * sizeof(float), this->raw_data_->data(), GL_STATIC_DRAW);
+
+        int attr_index = 0;
+
+        glVertexAttribPointer(attr_index, decltype(StaticMeshVertexData::Position)::row_count, GL_FLOAT, GL_FALSE, sizeof(StaticMeshVertexData::Position), (void*)offsetof(StaticMeshVertexData, Position));
+        glEnableVertexAttribArray(attr_index);
+        ++attr_index;
+
+        glVertexAttribPointer(attr_index, decltype(StaticMeshVertexData::Normal)::row_count, GL_FLOAT, GL_FALSE, sizeof(StaticMeshVertexData::Normal), (void*)offsetof(StaticMeshVertexData, Normal));
+        glEnableVertexAttribArray(attr_index);
+        ++attr_index;
+
+        glVertexAttribPointer(attr_index, decltype(StaticMeshVertexData::Tangent)::row_count, GL_FLOAT, GL_FALSE, sizeof(StaticMeshVertexData::Tangent), (void*)offsetof(StaticMeshVertexData, Tangent));
+        glEnableVertexAttribArray(attr_index);
+        ++attr_index;
+
+        glVertexAttribPointer(attr_index, decltype(StaticMeshVertexData::BitTangent)::row_count, GL_FLOAT, GL_FALSE, sizeof(StaticMeshVertexData::BitTangent), (void*)offsetof(StaticMeshVertexData, BitTangent));
+        glEnableVertexAttribArray(attr_index);
+        ++attr_index;
+
+        glVertexAttribPointer(attr_index, 2 * APATITE_STATICMESH_MAX_TEXTURE_COORDS, GL_FLOAT, GL_FALSE, sizeof(StaticMeshVertexData::Coords), (void*)offsetof(StaticMeshVertexData, Coords));
+        glEnableVertexAttribArray(attr_index);
+        ++attr_index;
+
+        glVertexAttribPointer(attr_index, 4, GL_FLOAT, GL_FALSE, sizeof(StaticMeshVertexData::VertColor), (void*)offsetof(StaticMeshVertexData, VertColor));
+        glEnableVertexAttribArray(attr_index);
+        ++attr_index;
+
+
+        glBindBuffer(GL_ARRAY_BUFFER, GL_NONE);
+        glBindVertexArray(GL_NONE);
+    }
+
+    void StaticMesh::UnBindGPU()
+    {
+        if (this->GetIsBindGPU())
+        {
+            glDeleteVertexArrays(1, &this->render_handle_);
+            glDeleteBuffers(1, &this->render_buffer_);
+
+            this->render_handle_ = 0;
+            this->render_buffer_ = 0;
+        }
+    }
+
+    bool StaticMesh::GetIsBindGPU()
+    {
+        return this->render_handle_;
+    }
+
+}
---
+++ Apatite/Assets/StaticMesh.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <Apatite/AssetObject.h>
+#include <Apatite/Math.h>
+#include <Apatite/Assets/Material.h>
+#include <Apatite/IBindGPU.h>
+
+namespace apatite
+{
+    class Texture2D;
+
+    constexpr static int APATITE_STATICMESH_MAX_TEXTURE_COORDS = 8;
+
+    //runtime vertex data
+    struct StaticMeshVertexData //128
+    {
+        Vector3f Position;  //12
+        Vector3f Normal;     //12
+        Vector3f Tangent;    //12
+        Vector3f BitTangent; //12
+        Vector2f Coords[APATITE_STATICMESH_MAX_TEXTURE_COORDS]; //8*n
+        LinearColorf VertColor; //16
+    };
+    using StaticMeshVertexDataArray = array_list<StaticMeshVertexData>;
+
+    ser::Stream& ReadWriteStream(ser::Stream& stream, bool is_write, StaticMeshVertexData& vert);
+
+    struct StaticMeshRawData
+    {
+        array_list<Vector3f> Positions;
+        array_list<Vector3f> Normals;
+        array_list<Vector3f> Tangent;
+        array_list<uint32_t> Indices;
+        array_list<LinearColorf> VertColors;
+        array_list<Vector2f> TexCoords[APATITE_STATICMESH_MAX_TEXTURE_COORDS];
+    };
+
+    class StaticMesh : public AssetObject, public IBindGPU
+    {
+        friend class StaticMeshAssetSerializer;
+        CORELIB_DEF_TYPE(AssemblyObject_Apatite, apatite::StaticMesh, AssetObject)
+    public:
+        constexpr static int32_t SerializeVersion = 1;
+        StaticMesh() {}
+        ~StaticMesh();
+    public://static
+        static sptr<StaticMesh> StaticCreate(sptr<StaticMeshVertexDataArray>&& managed_data);
+
+    public://member
+        virtual void Serialize(ser::Stream& stream, bool is_ser) override;
+        virtual void OnInstantiateAsset(sptr<AssetObject>& obj) override;
+        virtual void Render();
+    public:
+
+    public:
+        // IBindGPU
+        virtual void BindGPU() override;
+        virtual void UnBindGPU() override;
+        virtual bool GetIsBindGPU() override;
+    protected:
+        sptr<StaticMeshVertexDataArray> raw_data_ = nullptr;
+        array_list<uint32_t> indices;
+        uint32_t render_handle_ = 0;
+        uint32_t render_buffer_ = 0;
+    };
+    CORELIB_DECL_SHORTSPTR(StaticMesh);
+
+
+}
