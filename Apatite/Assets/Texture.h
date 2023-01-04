0bd1d5f92bf9eaf3fcf918f99a2a9548dfbe18bf
JomiXedYu <JomiXedYu@outlook.com>
2023-01-05 01:01:08 +0800

    Update Texture.h

--- Apatite/Assets/Texture.h
+++ Apatite/Assets/Texture.h
@@ -10,6 +10,8 @@ namespace apatite
 
     public:
         uint32_t get_tex_id() const { return tex_id_; }
+        virtual int32_t get_width() const = 0;
+        virtual int32_t get_height() const = 0;
 
     protected:
         uint32_t tex_id_ = 0;
