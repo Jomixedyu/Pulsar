fbaab3d71759447e4f854366a6e3c4db61e729f4
JomiXedYu <JomiXedYu@outlook.com>
2023-01-05 01:45:59 +0800

    textasset

---
+++ Apatite/Assets/TextAsset.cpp
@@ -0,0 +1,33 @@
+#include "TextAsset.h"
+#include "TextAsset.h"
+
+namespace apatite
+{
+
+    void TextAsset::Serialize(ser::Stream& stream, bool is_ser)
+    {
+        ser::ReadWriteStream(stream, is_ser, this->text_);
+    }
+
+    void TextAsset::OnInstantiateAsset(sptr<AssetObject>& obj)
+    {
+        auto i = sptr_cast<TextAsset>(obj);
+        i->text_ = this->text_;
+    }
+
+    sptr<TextAsset> TextAsset::StaticCreate(const string& str)
+    {
+        TextAsset* txt = new TextAsset;
+        txt->text_ = str;
+        return mksptr(txt);
+    }
+
+    sptr<TextAsset> TextAsset::StaticCreate(string&& str)
+    {
+        TextAsset* txt = new TextAsset;
+        txt->text_ = std::move(str);
+        return mksptr(txt);
+    }
+
+
+}
