cede7175090378c4e984444d463dd7a81ef87aff
JomiXedYu <JomiXedYu@outlook.com>
2023-01-10 01:42:41 +0800

    Update ImGuiImpl.cpp

--- Apatite/ImGuiImpl.cpp
+++ Apatite/ImGuiImpl.cpp
@@ -34,7 +34,7 @@ namespace apatite
         
         io.Fonts->AddFontFromFileTTF("DroidSans.ttf", 18.0f);
         ImGui::StyleColorsDark();
-        //ImGui::StyleColorsClassic();
+
         ImGuiStyle& style = ImGui::GetStyle();
         
         //custom
@@ -43,6 +43,8 @@ namespace apatite
             style.FramePadding = { 12,5 };
             style.ItemSpacing = { 10,8 };
             style.ScrollbarSize = 18;
+            style.Colors[ImGuiCol_::ImGuiCol_WindowBg] = ImColor(26, 26, 26, 255);
+            style.Colors[ImGuiCol_::ImGuiCol_FrameBg] = ImColor(12, 12, 12, 255);
             //bug:
             //style.WindowMenuButtonPosition = ImGuiDir_::ImGuiDir_Right;
         }
