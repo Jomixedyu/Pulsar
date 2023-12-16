dd14ca8073efaf132c7bfeddc5dc84e9ca1e4d9e
JomiXedYu <JomiXedYu@outlook.com>
2023-12-17 02:43:12 +0800

    Update FloatPropertyControl.cpp

--- Pulsar/Source/PulsarEd/src/PropertyControls/FloatPropertyControl.cpp
+++ Pulsar/Source/PulsarEd/src/PropertyControls/FloatPropertyControl.cpp
@@ -9,7 +9,7 @@ namespace pulsared
 
         auto f = static_cast<Single32*>(prop);
         ImGui::PushItemWidth(-1);
-        bool changed = ImGui::DragFloat(("##" + name).c_str(), &f->value, 0.2f);
+        bool changed = ImGui::DragFloat(("##" + name).c_str(), &f->value, 0.002f);
         ImGui::PopItemWidth();
         return changed;
     }
