49fee3be1218958e0d23e85952a013789472b4ec
JomiXedYu <JomiXedYu@outlook.com>
2024-03-03 21:15:41 +0800

    translate tool

--- Pulsar/Source/PulsarEd/include/PulsarEd/EdTools/MoveEdTool.cpp
+++ Pulsar/Source/PulsarEd/include/PulsarEd/EdTools/MoveEdTool.cpp
@@ -8,17 +8,15 @@
 
 namespace pulsared
 {
-    static ImGuizmo::OPERATION mCurrentGizmoOperation(ImGuizmo::TRANSLATE);
-    static ImGuizmo::MODE mCurrentGizmoMode(ImGuizmo::LOCAL);
-
     void MoveEdTool::Begin()
     {
         base::Begin();
-        Logger::Log("MoveTool");
     }
     void MoveEdTool::Tick(float dt)
     {
+        m_enableSelect = !ImGuizmo::IsOver();
         base::Tick(dt);
+
         if (EditorSelection::Selection.IsEmpty())
         {
             return;
@@ -27,15 +25,8 @@ namespace pulsared
         ImGuizmo::SetOrthographic(false);
         ImGuizmo::SetDrawlist();
 
-        static bool useSnap = false;
-        static float snap[3] = { 1.f, 1.f, 1.f };
-        static float bounds[] = { -0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.5f };
-        static float boundsSnap[] = { 0.1f, 0.1f, 0.1f };
-        static bool boundSizing = false;
-        static bool boundSizingSnap = false;
-
-        float windowWidth = (float)ImGui::GetWindowWidth();
-        float windowHeight = (float)ImGui::GetWindowHeight();
+        auto windowWidth = (float)ImGui::GetWindowWidth();
+        auto windowHeight = (float)ImGui::GetWindowHeight();
         ImGuizmo::SetRect(ImGui::GetWindowPos().x, ImGui::GetWindowPos().y, windowWidth, windowHeight);
 
         auto viewMat = m_world->GetPreviewCamera()->GetViewMat();
@@ -43,11 +34,23 @@ namespace pulsared
         auto node = EditorSelection::Selection.GetSelected();
         auto matrix = node->GetTransform()->GetLocalToWorldMatrix();
 
-        ImGuizmo::Manipulate(
-            viewMat.get_value_ptr(),
-            projMat.get_value_ptr(), mCurrentGizmoOperation, mCurrentGizmoMode, matrix.get_value_ptr(), NULL,
-        useSnap ? &snap[0] : NULL, boundSizing ? bounds : NULL, boundSizingSnap ? boundsSnap : NULL);
+        static ImGuizmo::MODE CurrentGizmoMode(ImGuizmo::WORLD);
 
-        //ImGuizmo::ViewManipulate(viewMat.get_value_ptr(), 1, ImVec2(2, 0), ImVec2(128, 128), 0x10101010);
+
+        if (ImGuizmo::Manipulate(
+            viewMat.get_value_ptr(),
+            projMat.get_value_ptr(),
+            ImGuizmo::TRANSLATE,
+            CurrentGizmoMode,
+            matrix.get_value_ptr(),
+            nullptr, nullptr, nullptr, nullptr))
+        {
+            node->GetTransform()->SetWorldPosition(matrix[3].xyz());
+        }
+
+        // ImGuizmo::Manipulate(
+        //     viewMat.get_value_ptr(),
+        //     projMat.get_value_ptr(), mCurrentGizmoOperation, mCurrentGizmoMode, matrix.get_value_ptr(), NULL,
+        // useSnap ? &snap[0] : NULL, boundSizing ? bounds : NULL, boundSizingSnap ? boundsSnap : NULL);
     }
 } // namespace pulsared
