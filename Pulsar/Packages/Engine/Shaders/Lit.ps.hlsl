5ec83083a4b715c86fd6d301f28b6be7b2ffec33
JomiXedYu <JomiXedYu@outlook.com>
2024-03-11 01:41:37 +0800

    lit

--- Pulsar/Packages/Engine/Shaders/Lit.ps.hlsl
+++ Pulsar/Packages/Engine/Shaders/Lit.ps.hlsl
@@ -3,17 +3,19 @@
 
 
 Texture2D _BaseColorTex : register(t0, space3);
-Texture2D _MRATex       : register(t1, space3);
+Texture2D _NormalMapTex : register(t1, space3);
+Texture2D _MRATex       : register(t2, space3);
 
-SamplerState _BaseColorSampler : register(s0, space3);
-SamplerState _MRASampler       : register(s1, space3);
+SamplerState _BaseColorSampler     : register(s0, space3);
+SamplerState _NormamlMapSampler    : register(s1, space3);
+SamplerState _MRAMapSampler        : register(s2, space3);
 
 
 MaterialAttributes SurfaceMain(InPixelAssembly surf)
 {
     MaterialAttributes attr = (MaterialAttributes)0;
     attr.BaseColor = _BaseColorTex.Sample(_BaseColorSampler, surf.TexCoord0);
-    float4 MRA = _MRATex.Sample(_MRASampler, surf.TexCoord0);
+    float4 MRA = _MRATex.Sample(_MRAMapSampler, surf.TexCoord0);
     attr.Metallic = MRA.r;
     attr.Roughness = MRA.g;
     attr.AmbientOcclusion = MRA.b;
