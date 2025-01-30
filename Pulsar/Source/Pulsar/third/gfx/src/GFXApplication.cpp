bca9e010387c06d4c4c67c8a61fac99995aea694
JomiXedYu <JomiXedYu@outlook.com>
2025-01-31 00:53:00 +0800

    gfx

--- Pulsar/Source/Pulsar/third/gfx/src/GFXApplication.cpp
+++
@@ -1,7 +0,0 @@
-#include <gfx/GFXApplication.h>
-
-
-gfx::GFXDescriptorSetLayout_sp gfx::GFXApplication::CreateDescriptorSetLayout(std::initializer_list<GFXDescriptorSetLayoutInfo> layouts)
-{
-    return CreateDescriptorSetLayout(layouts.begin(), layouts.size());
-}
