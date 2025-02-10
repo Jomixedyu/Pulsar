e553472605d9451f61a76c5aa7a6a2f62a154f90
JomiXedYu <JomiXedYu@outlook.com>
2025-02-11 00:17:57 +0800

    skinned mesh

--- Pulsar/Source/Pulsar/src/Components/SkinnedMeshRendererComponent.cpp
+++ Pulsar/Source/Pulsar/src/Components/SkinnedMeshRendererComponent.cpp
@@ -3,6 +3,12 @@
 namespace pulsar
 {
 
+    SkinnedMeshRendererComponent::SkinnedMeshRendererComponent()
+        : CORELIB_INIT_INTERFACE(IRendererComponent)
+    {
+        m_canDrawGizmo = true;
+    }
+
     void SkinnedMeshRendererComponent::BeginComponent()
     {
         base::BeginComponent();
@@ -11,6 +17,29 @@ namespace pulsar
     void SkinnedMeshRendererComponent::EndComponent()
     {
         base::EndComponent();
+    }
+    void SkinnedMeshRendererComponent::PostEditChange(FieldInfo* info)
+    {
+        base::PostEditChange(info);
+        if (info->GetName() == NAMEOF(m_skinnedMesh))
+        {
+            SetSkinnedMesh(m_skinnedMesh);
+        }
+    }
+    void SkinnedMeshRendererComponent::OnDrawGizmo(GizmoPainter* painter, bool selected)
+    {
+        base::OnDrawGizmo(painter, selected);
+
+    }
+    void SkinnedMeshRendererComponent::SetSkinnedMesh(const RCPtr<SkinnedMesh>& mesh)
+    {
+        if (m_skinnedMesh == mesh)
+        {
+            return;
+        }
+
+        m_skinnedMesh = mesh;
+        // recreate render object
 
     }
 } // namespace pulsar
