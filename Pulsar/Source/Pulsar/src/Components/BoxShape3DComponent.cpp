0c95704aee378d706e8460b9987d173287ec08af
JomiXedYu <JomiXedYu@outlook.com>
2025-02-11 22:55:07 +0800

    Update BoxShape3DComponent.cpp

--- Pulsar/Source/Pulsar/src/Components/BoxShape3DComponent.cpp
+++ Pulsar/Source/Pulsar/src/Components/BoxShape3DComponent.cpp
@@ -1,49 +1,27 @@
 #include "Components/BoxShape3DComponent.h"
 
 #include "Components/TransformComponent.h"
+#include "Rendering/SimplePrimitiveUtils.h"
 
 namespace pulsar
 {
-    template<typename _Vector>
-    static std::array<_Vector, 24> IdentityBox()
-    {
-        constexpr auto x = 0.5f;
-        constexpr auto y = 0.5f;
-        constexpr auto z = 0.5f;
-
-        auto a = _Vector{ -x,  y, -z };
-        auto b = _Vector{ -x,  y,  z };
-        auto c = _Vector{  x,  y,  z };
-        auto d = _Vector{  x,  y, -z };
-        auto e = _Vector{ -x, -y, -z };
-        auto f = _Vector{ -x, -y,  z };
-        auto g = _Vector{  x, -y,  z };
-        auto h = _Vector{  x, -y, -z };
-
-        return {
-            a,b, b,c, c,d, d,a,
-            e,f, f,g, g,h, h,e,
-            a,e, b,f, c,g, d,h
-        };
-    }
 
     void BoxShape3DComponent::OnDrawGizmo(GizmoPainter* painter, bool selected)
     {
-        static auto array = IdentityBox<Vector3f>();
-
-        StaticMeshVertex vertices[24];
-
-        auto local2World = GetTransform()->GetLocalToWorldMatrix();
-        auto color = selected ? GizmoPainter::DefaultSelectedLineColor : GizmoPainter::DefaultLineColor;
-
-        for (int i = 0; i < 24; ++i)
-        {
-            vertices[i].Position = local2World * array[i];
-            vertices[i].Color = color;
-        }
-
+        static auto array = SimplePrimitiveUtils::CreateBox<Vector3f>();
         if (selected)
         {
+            StaticMeshVertex vertices[24];
+
+            auto local2World = GetTransform()->GetLocalToWorldMatrix();
+            auto color = selected ? GizmoPainter::DefaultSelectedLineColor : GizmoPainter::DefaultLineColor;
+
+            for (int i = 0; i < 24; ++i)
+            {
+                vertices[i].Position = local2World * (array[i] * m_halfSize);
+                vertices[i].Color = color;
+            }
+
             painter->DrawLines(vertices, 24);
         }
     }
