fb0d960df8989ae9a32859416aaca50d0de53968
渔 <JomiXedYu@outlook.com>
2026-08-09 20:49:30 +0800

    Update Effects.inc.hlsl

--- Pulsar/Packages/Pulsar/Assets/Shaders/ShaderLibrary/Effects.inc.hlsl
+++ Pulsar/Packages/Pulsar/Assets/Shaders/ShaderLibrary/Effects.inc.hlsl
@@ -30,3 +30,25 @@ float TwoSideAlpha(float v, float offset = -0.5, float scale = 1, float softness
     float soft = softness * scale;
     return saturate(1 - smoothstep(start, start + soft, c));
 }
+
+
+half3 RimLight2(half NoL, half NoV, half NoLOffset = 0,
+  float frontWidth = 0.85, float frontSoftness = 0.05, half3 frontColor = half3(1, 0.75, 0.2), half frontIntensity = 1,
+  float backWidth = 0.85, float backSoftness = 0.05, half3 backColor = half3(0.3, 0.5, 0.7), half backIntensity = 1
+)
+{
+    NoL = NoL + NoLOffset;
+    half fr = saturate(1 - NoV);
+
+    float frontMin = saturate(frontWidth - frontSoftness);
+    float frontMax = saturate(frontWidth + frontSoftness);
+    half frontMask = (half)saturate((fr - frontMin) / (frontMax - frontMin)) * saturate(NoL);
+    half3 frontPart = frontColor * frontIntensity * frontMask;
+
+    float backMin = saturate(backWidth - backSoftness);
+    float backMax = saturate(backWidth + backSoftness);
+    half backMask = (half)saturate((fr - backMin) / (backMax - backMin)) * saturate(NoL * -1);
+    half3 backPart = backColor * backIntensity * backMask;
+
+    return frontPart + backPart;
+}
